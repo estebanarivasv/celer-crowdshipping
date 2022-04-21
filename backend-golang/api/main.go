6f5d43fe28b588f15ad63df9c4b57e70e049278d
Esteban Rivas <estebanarivasv@gmail.com>
2022-04-21 12:52:15 -0300

    feat: add tables generation at startup

--- backend-golang/api/main.go
+++ backend-golang/api/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/estebanarivasv/Celer/backend-golang/api/app/config"
 	"github.com/estebanarivasv/Celer/backend-golang/api/app/handlers"
+	"github.com/estebanarivasv/Celer/backend-golang/api/app/models"
 	"github.com/gin-gonic/gin"
 )
 
@@ -10,7 +11,10 @@ func main() {
 	router := gin.Default()
 
 	// Connect to database
-	config.ConnectToDb()
+	db := config.ConnectToDb()
+
+	// AutoMigrate will create tables, missing foreign keys, constraints, columns and indexes
+	db.AutoMigrate(&models.Shipping{})
 
 	// Load REST controllers
 	handlers.Routes(router)
