fef79a2bcb860491c74c71fa6cb582e90a5dab67
Esteban Rivas <estebanarivasv@gmail.com>
2022-04-21 12:48:58 -0300

    feat: associate shippings routes with handlers

--- backend-golang/api/app/handlers/routes.go
+++ backend-golang/api/app/handlers/routes.go
@@ -6,5 +6,10 @@ import (
 
 func Routes(router *gin.Engine) {
 	// Create a get method and associate it with the welcome function
-	router.GET("/", welcome)
+	router.GET("/", indexHandler)
+	router.POST("/sender/shippings", newShippingHandler)
+	router.GET("/sender/shippings", getAllShippingsHandler)
+	router.GET("/sender/shippings/:id", getShippingByIDHandler)
+	router.PUT("/sender/shippings/:id", updateShippingByIDHandler)
+	router.DELETE("/sender/shippings/:id", deleteShippingByIDHandler)
 }
