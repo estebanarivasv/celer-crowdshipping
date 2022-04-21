89a737ddeefda3dd40a0a5bda8344d8b0cb5d6c9
Esteban Rivas <estebanarivasv@gmail.com>
2022-04-21 12:52:57 -0300

    fix: change uint to int for simplicity

--- backend-golang/api/app/models/shipping.go
+++ backend-golang/api/app/models/shipping.go
@@ -2,7 +2,7 @@ package models
 
 type Shipping struct {
 	// TODO: Field-Level Permission gorm
-	ID              uint   `gorm:"primaryKey" json:"id"`
+	ID              int    `gorm:"primaryKey" json:"id"`
 	Image           string `json:"image"`
 	Details         string `json:"details"`
 	StartAddr       string `json:"start_addr"`
