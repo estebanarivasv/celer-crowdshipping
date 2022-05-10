7b91f3bc1a54a7cae214d92acbee7fce35447e84
Esteban Rivas <estebanarivasv@gmail.com>
2022-05-09 23:44:09 -0300

    feat: add camunda route handler

---
+++ backend-golang/api/app/handlers/test_route.go
@@ -0,0 +1,35 @@
+package handlers
+
+import (
+	"github.com/estebanarivasv/Celer/backend-golang/api/app/dtos"
+	"github.com/estebanarivasv/Celer/backend-golang/api/app/services"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func sendMessageToProc(c *gin.Context) {
+	var jsonMessage dtos.MessageToProc
+	if err := c.ShouldBindJSON(&jsonMessage); err != nil {
+		c.JSON(
+			http.StatusBadRequest,
+			gin.H{"error": err.Error()})
+		return
+	}
+	err := services.SendMessage(jsonMessage.MessageName, jsonMessage.ProcessInstanceId)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, err.Error())
+		return
+	}
+	c.JSON(http.StatusNoContent, http.NoBody)
+	return
+}
+
+func createShippingProc(c *gin.Context) {
+	var data, err = services.CreateShippingProcInstance()
+	if err != nil {
+		c.JSON(http.StatusBadRequest, err.Error())
+		return
+	}
+	c.JSON(http.StatusCreated, data)
+	return
+}
