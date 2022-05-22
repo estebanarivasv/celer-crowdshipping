f2883bc3f4cf3d6e0b29260d516e3ecda77fa06a
Esteban Rivas <estebanarivasv@gmail.com>
2022-05-22 12:02:44 -0300

    refactor: relocating mappers from model to mappers directory

---
+++ backend-golang/api/app/mappers/camunda.go
@@ -0,0 +1,17 @@
+package mappers
+
+import (
+	"encoding/json"
+	"github.com/estebanarivasv/Celer/backend-golang/api/app/dtos/camunda"
+	"log"
+)
+
+func JsonToShippingStruct(jsonResponse []byte) (*shipping.BasicShippingProc, error) {
+	data := new(shipping.BasicShippingProc)
+	err := json.Unmarshal(jsonResponse, &data)
+	if err != nil {
+		log.Fatal(err)
+		return nil, err
+	}
+	return data, nil
+}
--- backend-golang/api/app/mappers/shipping.go
+++ backend-golang/api/app/mappers/shipping.go
@@ -1,17 +1,62 @@
 package mappers
 
 import (
-	"encoding/json"
-	"github.com/estebanarivasv/Celer/backend-golang/api/app/dtos/camunda"
-	"log"
+	"github.com/estebanarivasv/Celer/backend-golang/api/app/dtos/entities"
+	"github.com/estebanarivasv/Celer/backend-golang/api/app/models"
 )
 
-func JsonToShippingStruct(jsonResponse []byte) (*shipping.BasicShippingProc, error) {
-	data := new(shipping.BasicShippingProc)
-	err := json.Unmarshal(jsonResponse, &data)
-	if err != nil {
-		log.Fatal(err)
-		return nil, err
+type ShippingMapper struct {
+}
+
+// FromDTO Return a model with the dto that comes from the service
+func (m ShippingMapper) FromDTO(dto *entities.ShippingInDTO) models.Shipping {
+
+	return models.Shipping{
+		Details:            dto.Details,
+		PackageID:          dto.PackageID,
+		SelectedOfferID:    dto.SelectedOfferID,
+		SenderID:           dto.SenderID,
+		RecipientID:        dto.RecipientID,
+		OriginAddress:      dto.OriginAddress,
+		DestinationAddress: dto.DestinationAddress,
 	}
-	return data, nil
+
+}
+
+// ToDTO Return a dto with the model that comes from the database
+func (m ShippingMapper) ToDTO(model *models.Shipping) entities.ShippingOutDTO {
+
+	// Each coordinate is mapped into a dto and appended to a slice
+	var routeArr []entities.RouteDetailOutDTO
+	for _, r := range model.Route {
+		routeArr = append(routeArr,
+			r.ToDTO())
+	}
+
+	// Each coordinate is mapped into a dto and appended to a slice
+	var offersSlice []entities.OfferOutDTO
+	for _, o := range model.Offers {
+		offersSlice = append(offersSlice,
+			o.ToDTO())
+	}
+
+	return entities.ShippingOutDTO{
+		ID:                 model.ID,
+		Details:            model.Details,
+		Package:            model.Package.ToDTO(),
+		OriginAddress:      model.OriginAddress,
+		DestinationAddress: model.DestinationAddress,
+		ProcessID:          model.ProcessID,
+		Offers:             offersSlice,
+		SelectedOffer:      model.SelectedOffer.ToDTO(),
+		Sender:             model.Sender.ToDTO(),
+		Recipient:          model.Recipient.ToDTO(),
+		Route:              routeArr,
+		PickedUpAt:         model.PickedUpAt,
+		DeliveredAt:        model.DeliveredAt,
+		CreatedAt:          model.CreatedAt,
+		UpdatedAt:          model.UpdatedAt,
+		DeletedAt:          model.DeletedAt.Time,
+	}
+
 }
