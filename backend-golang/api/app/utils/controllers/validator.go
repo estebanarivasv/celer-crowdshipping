7b208898afcfd496d8b009bf4b73117df6a0c4c0
Esteban Rivas <estebanarivasv@gmail.com>
2022-05-22 12:04:28 -0300

    feat: add conversor from str to int

--- backend-golang/api/app/utils/controllers/validator.go
+++ backend-golang/api/app/utils/controllers/validator.go
@@ -1,7 +1,11 @@
 package controllers
 
 import (
+	"github.com/estebanarivasv/Celer/backend-golang/api/app/dtos"
 	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+	"strconv"
 )
 
 // InDTO Generic interface that makes implement a generic method available for all the Controllers
@@ -10,10 +14,28 @@ type InDTO interface {
 }
 
 // ShouldBindDTO Generic method that maps the JSON body to the generic InputDTO defined below
-func ShouldBindDTO[T InDTO](c *gin.Context, DTO T) (boundDTO T, e error) {
-	err := c.ShouldBindJSON(&DTO)
+func ShouldBindDTO[T InDTO](c *gin.Context, dto T) (boundDTO T) {
+	err := c.ShouldBindJSON(&dto)
 	if err != nil {
-		return nil, err
+		c.JSON(
+			http.StatusBadRequest,
+			dtos.Response{Error: "the server will not process the request due to a malformed " +
+				"request syntax, an invalid request message framing, or a deceptive request routing"})
+		return
 	}
-	return DTO, nil
+	return dto
+}
+
+// IsZero compare if generic model is not null
+func IsZero[T comparable](v T) bool {
+	return v == *new(T)
+}
+
+func ConvertParamToInt(param string) (int, error) {
+	log.Printf(param)
+	p, err := strconv.Atoi(param)
+	if err != nil {
+		return 0, err
+	}
+	return p, nil
 }
