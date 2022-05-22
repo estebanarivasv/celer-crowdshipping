7ec477042d5348f08c7c82f659d85e8390866e53
Esteban Rivas <estebanarivasv@gmail.com>
2022-05-22 12:17:45 -0300

    feat: add link to docs

--- backend-golang/api/app/controllers/handlers/index.go
+++ backend-golang/api/app/controllers/handlers/index.go
@@ -8,7 +8,7 @@ import (
 func IndexHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"status":  200,
-		"message": "Welcome to celer-crowdshipping API",
+		"message": "Welcome to celer-crowdshipping API - Docs: http://localhost:5001/docs/swagger",
 	})
 	return
 }
