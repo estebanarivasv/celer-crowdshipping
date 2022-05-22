cdf0b1e9033db87f0a8d1d1cff05d2827600907e
Esteban Rivas <estebanarivasv@gmail.com>
2022-05-22 18:49:48 -0300

    fix: change dtos to interfaces

--- backend-golang/api/app/dtos/entities/offer.go
+++ backend-golang/api/app/dtos/entities/offer.go
@@ -11,13 +11,13 @@ type OfferInDTO struct {
 }
 
 type OfferOutDTO struct {
-	ID           int        `json:"id"`
-	ShippingCost float64    `json:"shipping_cost"`
-	Message      string     `json:"message"`
-	Duration     float64    `json:"duration"`
-	ShippingID   int        `json:"request_id"`
-	Distributor  UserOutDTO `json:"distributor"`
-	CreatedAt    time.Time  `json:"created_at"`
-	UpdatedAt    time.Time  `json:"updated_at"`
-	DeletedAt    time.Time  `json:"deleted_at"`
+	ID            int       `json:"id"`
+	ShippingCost  float64   `json:"shipping_cost"`
+	Message       string    `json:"message"`
+	Duration      float64   `json:"duration"`
+	ShippingID    int       `json:"request_id"`
+	DistributorID int       `json:"distributor_id"`
+	CreatedAt     time.Time `json:"created_at"`
+	UpdatedAt     time.Time `json:"updated_at"`
+	DeletedAt     time.Time `json:"deleted_at"`
 }
--- backend-golang/api/app/dtos/entities/shipping.go
+++ backend-golang/api/app/dtos/entities/shipping.go
@@ -18,20 +18,20 @@ type ShippingInDTO struct {
 }
 
 type ShippingOutDTO struct {
-	ID                 int                 `json:"id"`
-	Details            string              `json:"details"`
-	Package            PackageOutDTO       `json:"package"`
-	OriginAddress      string              `json:"origin_addr"`
-	DestinationAddress string              `json:"dest_addr"`
-	ProcessID          string              `json:"process_id"`
-	Offers             []OfferOutDTO       `json:"offers"`
-	SelectedOffer      OfferOutDTO         `json:"selected_offer"`
-	Sender             UserOutDTO          `json:"sender"`
-	Recipient          UserOutDTO          `json:"recipient"`
-	Route              []RouteDetailOutDTO `json:"route"`
-	PickedUpAt         time.Time           `json:"pickup_at"`
-	DeliveredAt        time.Time           `json:"delivered_at"`
-	CreatedAt          time.Time           `json:"created_at"`
-	UpdatedAt          time.Time           `json:"updated_at"`
-	DeletedAt          time.Time           `json:"deleted_at"`
+	ID                 int         `json:"id"`
+	Details            string      `json:"details"`
+	Package            interface{} `json:"package"`
+	OriginAddress      string      `json:"origin_addr"`
+	DestinationAddress string      `json:"dest_addr"`
+	ProcessID          string      `json:"process_id"`
+	SelectedOfferID    int         `json:"selected_offer_id"`
+	Sender             interface{} `json:"sender"`
+	Recipient          interface{} `json:"recipient"`
+	PickedUpAt         time.Time   `json:"pickup_at"`
+	DeliveredAt        time.Time   `json:"delivered_at"`
+	CreatedAt          time.Time   `json:"created_at"`
+	UpdatedAt          time.Time   `json:"updated_at"`
+	DeletedAt          time.Time   `json:"deleted_at"`
+	//Offers             []interface{} `json:"offers"`
+	//Route              []interface{} `json:"route"`
 }
