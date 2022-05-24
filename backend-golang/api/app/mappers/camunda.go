15c4439a90b6e6358836a6065f29a4be11708ffd
Esteban Rivas <estebanarivasv@gmail.com>
2022-05-24 01:46:56 -0300

    feat: add camunda mappers

--- backend-golang/api/app/mappers/camunda.go
+++ backend-golang/api/app/mappers/camunda.go
@@ -1,17 +1,31 @@
 package mappers
 
 import (
+	"bytes"
 	"encoding/json"
-	"github.com/estebanarivasv/Celer/backend-golang/api/app/dtos/camunda"
+	"github.com/estebanarivasv/Celer/backend-golang/api/app/dtos"
 	"log"
 )
 
-func JsonToShippingStruct(jsonResponse []byte) (*shipping.BasicShippingProc, error) {
-	data := new(shipping.BasicShippingProc)
+type CamundaMapper struct{}
+
+func (m *CamundaMapper) JsonBodyToCamundaProcessDTO(jsonResponse []byte) (*dtos.BasicCamundaProcessDTO, error) {
+	var data dtos.BasicCamundaProcessDTO
 	err := json.Unmarshal(jsonResponse, &data)
 	if err != nil {
 		log.Fatal(err)
 		return nil, err
 	}
-	return data, nil
+	return &data, nil
+}
+
+func (m *CamundaMapper) PayloadToJsonBody(payload interface{}) (*bytes.Buffer, error) {
+
+	encodedPayload, err := json.Marshal(payload)
+	if err != nil {
+		return nil, err
+	}
+	reader := bytes.NewBuffer(encodedPayload)
+
+	return reader, nil
 }
