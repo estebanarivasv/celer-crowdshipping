8838f8ade3c00e374dd1257153f4f42c05231619
Esteban Rivas <estebanarivasv@gmail.com>
2022-05-24 01:45:17 -0300

    feat: add put dto for updates

--- backend-golang/api/app/dtos/entities/shipping.go
+++ backend-golang/api/app/dtos/entities/shipping.go
@@ -7,14 +7,23 @@ import (
 type ShippingInDTO struct {
 	Details            string `json:"details"`
 	PackageID          int    `json:"package_id"`
+	SenderID           int    `json:"sender_id"`
+	RecipientID        int    `json:"recipient_id"`
+	OriginAddress      string `json:"origin_addr"`
+	DestinationAddress string `json:"dest_addr"`
+	// PickedUpAt         time.Time        `json:"pickup_at"` TODO DELETE
+	// DeliveredAt        time.Time        `json:"delivered_at"`
+	// Route              []RouteDetailDTO `json:"route"`
+}
+
+type ShippingInPutDTO struct {
+	Details            string `json:"details"`
+	PackageID          int    `json:"package_id"`
 	SelectedOfferID    int    `json:"selected_offer_id"`
 	SenderID           int    `json:"sender_id"`
 	RecipientID        int    `json:"recipient_id"`
 	OriginAddress      string `json:"origin_addr"`
 	DestinationAddress string `json:"dest_addr"`
-	// PickedUpAt         time.Time        `json:"pickup_at"`
-	// DeliveredAt        time.Time        `json:"delivered_at"`
-	// Route              []RouteDetailDTO `json:"route"`
 }
 
 type ShippingOutDTO struct {
@@ -24,7 +33,7 @@ type ShippingOutDTO struct {
 	OriginAddress      string      `json:"origin_addr"`
 	DestinationAddress string      `json:"dest_addr"`
 	ProcessID          string      `json:"process_id"`
-	SelectedOfferID    int         `json:"selected_offer_id"`
+	SelectedOfferID    *int        `json:"selected_offer_id"`
 	Sender             interface{} `json:"sender"`
 	Recipient          interface{} `json:"recipient"`
 	PickedUpAt         time.Time   `json:"pickup_at"`
@@ -32,6 +41,6 @@ type ShippingOutDTO struct {
 	CreatedAt          time.Time   `json:"created_at"`
 	UpdatedAt          time.Time   `json:"updated_at"`
 	DeletedAt          time.Time   `json:"deleted_at"`
-	//Offers             []interface{} `json:"offers"`
+	//Offers             []interface{} `json:"offers"` TODO DELETE
 	//Route              []interface{} `json:"route"`
 }
