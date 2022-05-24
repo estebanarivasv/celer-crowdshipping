95e7ff74ec908a7c24231aab49b47f9ea1bae908
Esteban Rivas <estebanarivasv@gmail.com>
2022-05-24 05:30:58 -0300

    refactor: move method to sender package

--- backend-golang/api/app/controllers/handlers/shipping.go
+++ backend-golang/api/app/controllers/handlers/shipping.go
@@ -7,41 +7,6 @@ import (
 	"net/http"
 )
 
-// GetShippingByID
-// @Summary Get Shipping
-// @Description Get Shipping stored in the database by passing an ID
-// @Consume application/json
-// @Accept json
-// @Produce json
-// @Param id path int true "Shipping ID"
-// @Success 201 {object} dtos.Response
-// @Failure 400 {object} dtos.Response
-// @Failure 404 {object} dtos.Response
-// @Router /sender/shippings/{id} [get]
-func GetShippingByID(c *gin.Context) {
-	id, err := controllers.ConvertParamToInt(c.Param("id"))
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, err)
-		return
-	}
-
-	dto := services.FindShippingById(id)
-	if !dto.Success {
-		if dto.Error == "record not found" {
-			c.JSON(http.StatusNotFound, dto)
-			return
-		}
-		c.JSON(http.StatusInternalServerError, dto)
-		return
-	}
-	c.JSON(http.StatusOK, dto)
-	return
-}
-
-func UpdateShippingStateByID(c *gin.Context) {
-
-}
-
 // GetShippingStateByID
 // @Summary Get Shipping state
 // @Description Get Shipping state making reference to Camunda instance
