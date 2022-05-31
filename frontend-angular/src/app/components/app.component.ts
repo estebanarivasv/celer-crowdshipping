95ee9659279832b9936a2aaf2d94591a4c96e4a4
Esteban <estebanarivasv@gmail.com>
2022-05-31 15:40:56 -0300

    feat: add header and footer components

--- frontend-angular/src/app/components/app.component.ts
+++ frontend-angular/src/app/components/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {SenderService} from "../services/sender.service";
+import {Shipping} from "../models/shipping";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'celer-client';
 }
