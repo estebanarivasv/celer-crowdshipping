42ffcb22005475f4d32ea1ed4c889f52e04f5f0e
Esteban <estebanarivasv@gmail.com>
2022-06-01 18:46:44 -0300

    refactor: delete footer

--- frontend-angular/src/app/app.module.ts
+++ frontend-angular/src/app/app.module.ts
@@ -2,25 +2,29 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {AppRoutingModule} from './app-routing.module';
 import {HttpClientModule} from "@angular/common/http";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {SharedModule} from "./components/shared.module";
 
 import {AppComponent} from './components/app.component';
 import {SenderComponent} from './components/views/sender/sender.component';
 import {HeaderComponent} from './components/header/header.component';
-import {FooterComponent} from './components/footer/footer.component';
-import {SharedModule} from "./components/shared.module";
+import {DistributorComponent} from "./components/views/distributor/distributor.component";
+import {DashboardComponent} from "./components/views/dashboard/dashboard.component";
 
 @NgModule({
     declarations: [
         AppComponent,
         SenderComponent,
+        DistributorComponent,
         HeaderComponent,
-        FooterComponent
+        DashboardComponent
     ],
     imports: [
         BrowserModule,
+        BrowserAnimationsModule,
         HttpClientModule,
         AppRoutingModule,
-        SharedModule
+        SharedModule,
     ],
     providers: [],
     bootstrap: [AppComponent]
