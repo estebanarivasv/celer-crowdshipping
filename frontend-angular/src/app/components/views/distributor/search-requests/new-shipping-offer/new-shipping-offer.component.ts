383befa4d4a7b610e1fba93e9c00745e51cfca76
Esteban <estebanarivasv@gmail.com>
2022-06-03 20:37:49 -0300

    feat: user can offer to a shipping request

---
+++ frontend-angular/src/app/components/views/distributor/search-requests/new-shipping-offer/new-shipping-offer.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, ParamMap} from "@angular/router";
+import {MessageService} from "primeng/api";
+import {Package} from "../../../../../models/package";
+import {Offer} from "../../../../../models/offer";
+import {ApiResponse} from "../../../../../models/response";
+import {DistributorService} from "../../../../../services/distributor.service";
+
+@Component({
+  selector: 'app-new-shipping-offer',
+  templateUrl: './new-shipping-offer.component.html',
+  providers: [MessageService]
+})
+export class NewShippingOfferComponent implements OnInit {
+
+  offerModel = new Offer();
+  saveOfferClicked = false;
+  requestId?: number;
+
+  constructor(private route: ActivatedRoute,
+              private messageService: MessageService,
+              private distributorService: DistributorService) { }
+
+  loadParamFromUrl() {
+    this.route.paramMap.subscribe((params: ParamMap) => this.requestId = parseInt(<string>params.get("id")));
+  }
+
+  ngOnInit(): void {
+    this.loadParamFromUrl();
+  }
+
+  onClickAddOffer() {
+    this.saveOfferClicked = true;
+    this.distributorService.makeOfferToRequest(this.requestId!, this.offerModel)
+        .subscribe(
+            (res: ApiResponse<Package>) => {
+              this.messageService.add({
+                severity: 'success',
+                summary: 'Created',
+                detail: 'Offer successfully made'
+              });
+            }, (error) => {
+              this.messageService.add({
+                severity: 'danger',
+                summary: error.summary,
+                detail: error.detail
+              });
+            });
+  }
+
+}
