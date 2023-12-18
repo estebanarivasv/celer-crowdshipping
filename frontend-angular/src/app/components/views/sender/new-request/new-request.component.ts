57b303b07f9c7a8959dd8bfa71aa9d945dae735d
Esteban <estebanarivasv@gmail.com>
2023-12-17 23:45:45 -0300

    fix: new shipping request view

--- frontend-angular/src/app/components/views/sender/new-request/new-request.component.ts
+++ frontend-angular/src/app/components/views/sender/new-request/new-request.component.ts
@@ -1,79 +1,103 @@
-import {Component, OnInit} from '@angular/core';
-import {Package} from "../../../../models/package";
-import {MessageService} from "primeng/api";
-import {PackageService} from "../../../../services/package.service";
-import {ApiResponse} from "../../../../models/response";
-import {Shipping} from "../../../../models/shipping";
-import {SenderService} from "../../../../services/sender.service";
-import {Router} from "@angular/router";
+import { Component, OnInit } from "@angular/core";
+import { Package } from "../../../../models/package";
+import { MessageService } from "primeng/api";
+import { PackageService } from "../../../../services/package.service";
+import { ApiResponse } from "../../../../models/response";
+import { Shipping } from "../../../../models/shipping";
+import { SenderService } from "../../../../services/sender.service";
+import { Router } from "@angular/router";
 
 @Component({
-    selector: 'app-new-request',
-    templateUrl: './new-request.component.html',
-    providers: [MessageService]
+  selector: "app-new-request",
+  templateUrl: "./new-request.component.html",
+  providers: [MessageService],
 })
 export class NewRequestComponent implements OnInit {
+  package = new Package();
+  shipping = new Shipping();
 
-    constructor(
-        private messageService: MessageService,
-        private packageService: PackageService,
-        private senderService: SenderService,
-        private router: Router,
-    ) {
-    }
+  constructor(
+    private messageService: MessageService,
+    private packageService: PackageService,
+    private senderService: SenderService,
+    private router: Router,
+  ) {}
 
-    packageModel = new Package();
-    packageFromApi = new Package();
-    savePkgClicked = false;
-    saveShipClicked = false;
-    dimensionsOpts: string[] = [
-        "Bag - <2 Kg",
-        "XS Box - 2 Kg",
-        "S Box - 3 Kg",
-        "M Box - 5 Kg",
-        "L Box - 10 Kg",
-        "XL Box - 20 Kg"
-    ];
+  dimensionsOpts: string[] = [
+    "Bag - <2 Kg",
+    "XS Box - 2 Kg",
+    "S Box - 3 Kg",
+    "M Box - 5 Kg",
+    "L Box - 10 Kg",
+    "XL Box - 20 Kg",
+  ];
 
-    shippingModel = new Shipping();
 
-    ngOnInit(): void {
-    }
 
-    onClickAddPackage() {
-        this.savePkgClicked = true;
-        this.packageService.addPackage(this.packageModel)
-            .subscribe(
-            (res: ApiResponse<Package>) => {
-                this.messageService.add({
-                    severity: 'success',
-                    summary: 'Created',
-                    detail: 'Package successfully created'
-                });
-                this.packageFromApi = res.data;
-            }, (error) => {
-                this.messageService.add({
-                    severity: 'danger',
-                    summary: error.summary,
-                    detail: error.detail
-                });
+  ngOnInit(): void {}
+
+  onSubmit(packageData: Package, shippingData: Shipping) {
+    console.log({ package: packageData, shipping: shippingData });
+    this.packageService.addPackage(packageData).subscribe({
+      next: (res: ApiResponse<Package>) => {
+        const packageFromApi = res.data;
+        shippingData.packageId = packageFromApi.id
+
+        this.senderService.addShipping(shippingData).subscribe({
+          next: (_res: ApiResponse<Shipping>) => {
+            this.messageService.add({
+              severity: 'success',
+              summary: 'Created',
+              detail: 'Shipping successfully created'
             });
-    }
+          },
+          error: (error) => {
+            this.messageService.add({
+              severity: "danger",
+              summary: error.summary,
+              detail: error.detail,
+            });
+          },
+        })
+      },
+      error: (error) => {
+        this.messageService.add({
+          severity: "danger",
+          summary: error.summary,
+          detail: error.detail,
+        });
+      },
+    });
 
+    /*
+    * this.authService.login(username, password).subscribe({
+      next: (response: any) => {
+        const jwtToken = response.data.token;
 
-    onClickAddShipping() {
-        this.saveShipClicked = true;
-        this.shippingModel.packageId = this.packageFromApi.id;
-        this.senderService.addShipping(this.shippingModel)
-            .subscribe(
-                (res: ApiResponse<Shipping>) => {
+        this.authService.setToken(jwtToken);
+        this.router.navigate(["/dashboard"]);
+      },
+      error: (error) => {
+        this.toastr.error(error.error.message, "", {
+          closeButton: true,
+        });
+      },
+    });
+    * */
+  }
+
+  /*
+        onClickAddPackage() {
+            this.savePkgClicked = true;
+            this.packageService.addPackage(this.packageModel)
+                .subscribe(
+                (res: ApiResponse<Package>) => {
                     this.messageService.add({
                         severity: 'success',
                         summary: 'Created',
-                        detail: 'Shipping successfully created'
+                        detail: 'Package successfully created'
                     });
-                    this.shippingModel = res.data;
-                    this.router.navigate([`sender/requests/${this.shippingModel.id}`]);
+                    this.packageFromApi = res.data;
                 }, (error) => {
                     this.messageService.add({
                         severity: 'danger',
@@ -81,5 +105,30 @@ export class NewRequestComponent implements OnInit {
                         detail: error.detail
                     });
                 });
-    }
+        }
+
+
+        onClickAddShipping() {
+            this.saveShipClicked = true;
+            this.shippingModel.packageId = this.packageFromApi.id;
+            this.senderService.addShipping(this.shippingModel)
+                .subscribe(
+                    (res: ApiResponse<Shipping>) => {
+                        this.messageService.add({
+                            severity: 'success',
+                            summary: 'Created',
+                            detail: 'Shipping successfully created'
+                        });
+                        this.shippingModel = res.data;
+                        this.router.navigate([`sender/requests/${this.shippingModel.id}`]);
+                    }, (error) => {
+                        this.messageService.add({
+                            severity: 'danger',
+                            summary: error.summary,
+                            detail: error.detail
+                        });
+                    });
+        }
+
+         */
 }
