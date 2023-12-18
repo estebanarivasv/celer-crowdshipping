ce4cd97d1e71e819cabd99077eeb73cfbf2531c6
Esteban <estebanarivasv@gmail.com>
2023-12-17 23:45:02 -0300

    feat: add new styling to cards

--- frontend-angular/src/app/components/views/dashboard/dashboard.component.ts
+++ frontend-angular/src/app/components/views/dashboard/dashboard.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html'
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent {
 
-  constructor() { }
+  constructor(private router: Router) { }
 }
